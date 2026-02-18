374232f80bcb39bece55ef718372eee8fa7fdd7e
Yash12122004 <yashwanthd8@gmail.com>
2026-02-19 01:37:04 +0530

    deployment parameters

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://eyva-assignment-1.onrender.com",
   credentials: true,
 }));
 
